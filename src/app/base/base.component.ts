ed743a71b743c4358362d27d7cef1836cba650e0
Muaaz Altahan <muaazaltahan@gmail.com>
2022-01-29 22:51:54 +0200

    Add Base Component

--- src/app/base/base.component.ts
+++ src/app/base/base.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { faFacebook, faGithub, faInstagram, faTelegram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-base',
   templateUrl: './base.component.html',
   styleUrls: ['./base.component.scss']
 })
-export class BaseComponent {}
+export class BaseComponent {
+
+  facebook = faFacebook;
+  instagram = faInstagram;
+  telegram = faTelegram;
+  github = faGithub;
+
+}
