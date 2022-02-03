45d288592f0aebda78e41d340331e662a89635c1
Muaaz Altahan <muaazaltahan@gmail.com>
2022-02-03 23:27:16 +0200

    Add Selected-Test Component

--- src/app/views/selected-test/selected-test.component.ts
+++ src/app/views/selected-test/selected-test.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Test } from 'src/app/models/test';
+import { getSelectedTest } from 'src/app/state/selected-test/reducer';
 
 @Component({
   selector: 'app-selected-test',
   templateUrl: './selected-test.component.html',
   styleUrls: ['./selected-test.component.scss']
 })
-export class SelectedTestComponent implements OnInit {
+export class SelectedTestComponent {
 
-  constructor() { }
+  constructor(private store: Store) { }
 
-  ngOnInit(): void {
-  }
+  test: Observable<Test> = this.store.select(getSelectedTest);
 
 }
--- src/app/views/selected-test/selected-test.module.ts
+++ src/app/views/selected-test/selected-test.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectedTestComponent } from './selected-test.component';
-
-
+import { StoreModule } from '@ngrx/store';
+import { reducer } from 'src/app/state/selected-test/reducer';
 
 @NgModule({
   declarations: [
     SelectedTestComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forFeature('selectedTest',reducer)
+  ],
+  exports: [
+    SelectedTestComponent
   ]
 })
 export class SelectedTestModule { }
