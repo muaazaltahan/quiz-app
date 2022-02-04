691d0771387170fa2a10d8ccd11ac0c198497fb6
Muaaz Altahan <muaazaltahan@gmail.com>
2022-02-04 20:43:29 +0200

    Add Timer

--- src/app/views/selected-test/selected-test.component.ts
+++ src/app/views/selected-test/selected-test.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription, timer } from 'rxjs';
 import { Test } from 'src/app/models/test';
 import { getSelectedTest } from 'src/app/state/selected-test/reducer';
 
@@ -9,10 +9,28 @@ import { getSelectedTest } from 'src/app/state/selected-test/reducer';
   templateUrl: './selected-test.component.html',
   styleUrls: ['./selected-test.component.scss']
 })
-export class SelectedTestComponent {
+export class SelectedTestComponent implements OnInit,OnDestroy {
 
   constructor(private store: Store) { }
 
+  timerSub: Subscription;
+  timer = timer(0,1000);
+  time = 0;
+
   test: Observable<Test> = this.store.select(getSelectedTest);
+
+  startTimer() {
+    this.timerSub = this.timer.subscribe(() => {
+      this.time += 1000
+    });
+  }
+
+  ngOnInit(): void {
+    this.startTimer();
+  }
+
+  ngOnDestroy(): void {
+    this.timerSub.unsubscribe();
+  }
 
 }
